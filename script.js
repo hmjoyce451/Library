ab7ebb434574f7d97dd5b659cfc0eb87fe9a4c93
hayden <hmjoyce451@gmail.com>
2021-04-06 15:54:42 -0700

    Added local storage functionality

--- script.js
+++ script.js
@@ -6,10 +6,24 @@ const pagesInput = document.getElementById('pages-input');
 
 let myLibrary = [];
 
-function Book (title, author, pages) {
+window.addEventListener('load', () => {
+    let storedLibrary = localStorage.getItem('library');
+    if(storedLibrary !== null) {
+        myLibrary = JSON.parse(storedLibrary);
+        displayBooks();
+    }
+});
+
+//update local storage function
+function updateLocalStorage() {
+    localStorage.setItem('library', JSON.stringify(myLibrary));  
+}
+
+function Book (title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
+    this.read = read;
 }
 
 //add books to Library
@@ -18,10 +32,12 @@ addBookBtn.addEventListener('click', e => {
     const title = titleInput.value;
     const author = authorInput.value;
     const pages = pagesInput.value;
+    const read = false;
+
     if(title === '' || author === '' || pages === '') {
         alert('please fill in all input fields');
     } else {
-        myLibrary.push(new Book(title, author, pages));
+        myLibrary.push(new Book(title, author, pages, read));
     } 
 
     displayBooks();
@@ -37,23 +53,26 @@ function displayBooks() {
     cardContainer.innerHTML = '';
     for(let i=0; i<myLibrary.length; i++) {
 
+        
+
         const bookCard = document.createElement('div');
         const bookTitle = document.createElement('p');
         const bookAuthor = document.createElement('p');
         const bookPages = document.createElement('p');
-        const bookRead = document.createElement('p');
 
 
-        bookRead.textContent = myLibrary[i].read;
+
         bookPages.textContent = `${myLibrary[i].pages} pages.`;
         bookAuthor.textContent = `Written by ${myLibrary[i].author}`;
         bookTitle.textContent = myLibrary[i].title;
+        
 
         cardContainer.appendChild(bookCard);
         bookCard.appendChild(bookTitle);
         bookCard.appendChild(bookAuthor);
         bookCard.appendChild(bookPages);
-        bookCard.appendChild(bookRead);
+
+
 
         //add remove button
         const removeBtn = document.createElement('button');
@@ -70,31 +89,39 @@ function displayBooks() {
                 } else if (myLibrary.length === 1) {
                     myLibrary = [];
                 }
-                console.log(myLibrary)
+                console.log(myLibrary);
+                displayBooks();
+                updateLocalStorage();
             }
         });
+
         //toggle button
-        const readToggle = document.createElement('input');
-        readToggle.type = 'checkbox';
+        const readToggle = document.createElement('button');
+        readToggle.innerText = 'Finished?';
         readToggle.classList.add('read-toggle');
         bookCard.appendChild(readToggle);
 
         readToggle.addEventListener('click', e => {
             bookCard.classList.toggle('finished');
+            if(bookCard.classList.contains('finished')){
+                myLibrary[i].read = true;
+            } else {
+                myLibrary[i].read = false;
+            }
+            console.log(myLibrary);
+            updateLocalStorage();
         });
-
-        //add text to checkbox
-        const finishedReading = document.createElement('p');
-        finishedReading.textContent = 'Finished?';
-        finishedReading.classList.add('toggle-text');
-        bookCard.appendChild(finishedReading);
 
         //add classes for styling
         bookCard.classList.add('book-card');
         bookTitle.classList.add('book-title');
         bookAuthor.classList.add('book-author');
         bookPages.classList.add('book-pages');
-        bookRead.classList.add('book-read');
-    }
-}
 
+        if(myLibrary[i].read === true) {
+            bookCard.classList.add('finished')
+        }
+    }
+    updateLocalStorage();
+    console.log(myLibrary)
+}
